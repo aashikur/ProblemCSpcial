d2a85fd2f1e418838efdb7ab42a506a47e99e150
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-05-19 16:15:53 +0600

    Create Array Intersection.c
    Can't ignore Double or Copy Element! :(

---
+++ Array Intersection.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int size1, size2;
+
+    printf("1st Array Size = "); // Getting First Array
+        scanf("%d", &size1);
+
+    int array1[size1];
+    printf("  Enter 1st Array: ");
+    for(int i = 0; i<size1; i++)
+        scanf("%d",&array1[i]); // 1st End
+
+
+    printf("2nd Array Size = "); // Getting 2nd Arry
+        scanf("%d", &size2);
+
+    int array2[size2];
+    printf("  Enter 2nd Array: ");
+    for(int i = 0; i<size2; i++)
+        scanf("%d",&array2[i]); // 2nd End
+
+
+    int x = size1>size2? size1: size2; // Dynamic size (Intersection);
+    int array3[x];
+    int size3 = 0;
+
+    for(int i=0; i<5; i++)  // Adding Value to Intersection Arry
+    {
+        for(int j=0; j<5; j++)
+        {
+            if(array1[i]==array2[j])
+            {
+                array3[size3++] = array1[i];
+                break;
+            }
+        }
+    }
+
+    printf("\nIntersection of Arrays: ");
+    for(int i = 0; i<size3; i++)
+        printf("%d ",array3[i]); // Printing 3rd array
+
+    printf("\n");
+
+
+    return 0;
+}
