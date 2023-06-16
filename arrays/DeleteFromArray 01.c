a5455b8957866d3f8016a1b59f195a10a64a6544
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-06-16 21:48:15 +0600

    Create DeleteFromArray 01.c
    Delete all Even from array

---
+++ arrays/DeleteFromArray 01.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define size 8
+
+int main(){
+
+    int arr[size] = {1,1,2,2,2,4,5,5};
+    int s = size;
+    int check  = 0;
+
+
+    for(int i = 0; i<s; i++)
+    {
+        if(arr[i]%2==0){
+            for(int j = i; j<s-1; j++)
+            {
+                arr[j] = arr[j+1];
+                check++;
+            }
+            s--;
+            i--;
+        }
+    }
+
+
+    for(int i =0;i<s;i++)
+        printf("%d ",arr[i]);
+
+return 0;
+
+}
